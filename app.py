339b547d76371884945e581dbd659d383eef7aef
ABorden23 <alexborden2@gmail.com>
2022-08-09 19:19:02 -0400

    updates to app.py

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
+#Dependencies 
 import datetime as dt
 import numpy as np
 import pandas as pd
 
+#Dependencies for SQAlchemy
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+#Dependencies for Flask
 from flask import Flask, jsonify
 
+#Access the SQLite Database
 engine = create_engine("sqlite:///hawaii.sqlite")
 
+#Function to access and query SQLite database
 Base = automap_base()
 
+#Reflect Database
 Base.prepare(engine, reflect=True)
 
+#Save Refrencest to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
+#Create Session Link
 session = Session(engine)
 
+#Set Up Flask
 app = Flask(__name__)
 
-import app
-
-print("example __name__ = %s", __name__)
-
-if __name__ == "__main__":
-    print("example is being run directly.")
-else:
-    print("example is being imported")
+#Define Welcome Route 
+@app.route('/')
 
 def welcome():
     return(
@@ -41,3 +44,21 @@ def welcome():
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
     ''')
+
+#Define Precipitation Route
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+# Defube Statuins Route
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
